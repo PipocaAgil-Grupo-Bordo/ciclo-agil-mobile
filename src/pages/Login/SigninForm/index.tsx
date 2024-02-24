539b6f12b55552232a20fe7710e9f7439ae7fc64
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-24 11:27:55 -0300

    imports Montserrt font

--- src/pages/Login/SigninForm/index.tsx
+++ src/pages/Login/SigninForm/index.tsx
@@ -7,11 +7,11 @@ const SigninForm: React.FC = () => {
   return (
     <View>
       <View>
-        <Text>Email:</Text>
+        <Text style={{ fontFamily: "Montserrat" }}>Email:</Text>
         <TextInput keyboardType="email-address" textAlign="center" />
-        <Text>Senha:</Text>
+        <Text style={{ fontFamily: "Montserrat" }}>Senha:</Text>
         <TextInput secureTextEntry={true} textAlign="center" />
-        <Text>Esqueci a senha</Text>
+        <Text style={{ fontFamily: "Montserrat" }}>Esqueci a senha</Text>
         <Button title="Login"></Button>
       </View>
     </View>
