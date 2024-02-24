5ac1e900cd3d9cdb7559e30a17f8880bc25911aa
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-24 10:18:38 -0300

    implements and renders SigninForm on app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Login/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import SigninForm from "./SigninForm";
+// import { Container } from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <View>
+      <SigninForm />
+    </View>
+  );
+};
+
+export default Login;
