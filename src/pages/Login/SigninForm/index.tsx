b532d0dacc9cc1fef0ce6f45aaab1296eeb4e8f2
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-27 09:26:30 -0300

    implements TouchableOpacity instead of Button

--- src/pages/Login/SigninForm/index.tsx
+++ src/pages/Login/SigninForm/index.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 // import { Container } from './styles';
 
 const SigninForm: React.FC = () => {
   return (
     <View>
-      <View>
+      <View >
         <Text style={{ fontFamily: "Montserrat" }}>Email:</Text>
         <TextInput keyboardType="email-address" textAlign="center" />
         <Text style={{ fontFamily: "Montserrat" }}>Senha:</Text>
         <TextInput secureTextEntry={true} textAlign="center" />
         <Text style={{ fontFamily: "Montserrat" }}>Esqueci a senha</Text>
-        <Button title="Login"></Button>
+        <TouchableOpacity>
+          <Text style={{ fontFamily: "Montserrat" }}>Login</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
