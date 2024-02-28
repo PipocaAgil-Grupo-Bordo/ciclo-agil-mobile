529c13c80809353777e8c25d095a13fb3729ead6
PedroRondelli <pedrorondelli@hotmail.com>
2024-02-28 07:46:28 -0300

    implements login form validation

---
+++ src/schemas/loginSchema.tsx
@@ -0,0 +1,12 @@
+import * as yup from "yup"
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("O email não pode ser vazio")
+    .email("Digite um email válido"),
+  password: yup
+    .string()
+    .required("A senha não pode ser vazia")
+    .min(6, "A senha deve conter pelo menos 6 dígitos"),
+});
