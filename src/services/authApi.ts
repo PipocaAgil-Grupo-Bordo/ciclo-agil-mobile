00cd6c97f5bc309be68a022995577eb11522a160
Luciano <ljr.luciano.junior@outlook.com>
2024-03-01 22:17:30 -0300

    fix: Correct typo

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -2,8 +2,8 @@ import { loginObject } from "../types/loginType";
 import api from "./api"
 
 function signInUser(body: loginObject) {
-    const promisse = api.post("/auth/users/signin", body);
-    return promisse;
+    const promise = api.post("/auth/users/signin", body);
+    return promise;
   }
 
 const authApi = {
