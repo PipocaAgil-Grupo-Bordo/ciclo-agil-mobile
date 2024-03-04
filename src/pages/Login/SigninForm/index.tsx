fdf52e118372e14902a31c13c47d1e8ff2f7260d
PedroRondelli <pedrorondelli@hotmail.com>
2024-03-04 14:05:04 -0300

    fixes bugs on login screen and add signin validation to login form

--- src/pages/Login/SigninForm/index.tsx
+++ src/pages/Login/SigninForm/index.tsx
@@ -1,54 +1,48 @@
 import React, { useEffect } from "react";
 import { Alert, Text } from "react-native";
-import { ForgotPasswordText, FormBox, FormButton, LoginInput } from "./style";
-import { SubmitHandler, UseFormReset, useForm } from "react-hook-form";
+import { ForgotPasswordText, FormBox, FormButton, LetsBegin, RegisterLink, RegisterText } from "./style";
+import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "../../../schemas/loginSchema";
 import { loginObject } from "../../../types/loginType";
-
-const onSubmit: SubmitHandler<loginObject> = ({ email, password }) => {
-  Alert.alert(email, password);
-};
+import Input from "../../../components/Input";
 
 const SigninForm: React.FC = () => {
   const {
-    register,
-    setValue,
     handleSubmit,
+    control,
     formState: { errors },
+    reset,
+    setError,
   } = useForm<loginObject>({
     resolver: yupResolver(loginSchema),
   });
+  const onSubmit: SubmitHandler<loginObject> = ({ email, password }) => {
+    Alert.alert(email, password);
+  };
+  const onDeniedSubmit: SubmitErrorHandler<loginObject> = ({
+    email,
+    password,
+  }) => {
+    reset({ email: "", password: "" }, { keepErrors: true });
+    setError("root", { type: "submit error" });
+    Alert.alert("deu erro");
+  };
 
   return (
     <FormBox>
+      <LetsBegin>Vamos começar?</LetsBegin>
       <Text style={{ fontFamily: "Montserrat" }}>Email:</Text>
-      <LoginInput
-        onChangeText={(text) => setValue("email", text)}
-        keyboardType="email-address"
-        textAlign="center"
-        {...register("email")}
-        error={errors.email}
-        placeholder={errors.email ? errors.email.message : ""}
-        placeholderTextColor={errors.email ? "#FF0000" : ""}
-      />
-
+      <Input name="email" control={control} errors={errors} />
       <Text style={{ fontFamily: "Montserrat" }}>Senha:</Text>
-      <LoginInput
-        onChangeText={(text) => setValue("password", text)}
-        secureTextEntry={true}
-        error={errors.password}
-        placeholder={errors.password ? errors.password.message : ""}
-        placeholderTextColor={errors.password ? "#FF0000" : ""}
-        textAlign="center"
-        {...register("password")}
-      />
+      <Input name="password" control={control} errors={errors} />
       <ForgotPasswordText style={{ fontFamily: "Montserrat" }}>
         Esqueci a senha
       </ForgotPasswordText>
-      <FormButton onPress={handleSubmit(onSubmit)}>
+      <FormButton onPress={handleSubmit(onSubmit, onDeniedSubmit)}>
         <Text style={{ fontFamily: "Montserrat", color: "white" }}>Login</Text>
       </FormButton>
+      <RegisterText >Não tem conta?<RegisterLink > Registre-se</RegisterLink></RegisterText>
     </FormBox>
   );
 };
