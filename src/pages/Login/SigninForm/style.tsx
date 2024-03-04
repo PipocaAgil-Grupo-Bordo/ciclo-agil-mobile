61051f5ad6a604de94cc68e182c3324b9673194b
PedroRondelli <pedrorondelli@hotmail.com>
2024-03-04 14:05:04 -0300

    fixes bugs on login screen and add signin validation to login form

--- src/pages/Login/SigninForm/style.tsx
+++ src/pages/Login/SigninForm/style.tsx
@@ -1,15 +1,6 @@
 import { FieldError } from "react-hook-form";
 import styled from "styled-components/native";
 
-export const LoginInput = styled.TextInput<{ error?: FieldError }>`
-  background-color: #f5f9ff;
-  border-radius: 5px;
-  height: 56px;
-  margin: 12px;
-  border: ${(props) => (props.error ? "#FF0000" : "")};
-  
-`;
-
 export const FormBox = styled.View``;
 export const FormButton = styled.TouchableOpacity`
   display: flex;
@@ -25,3 +16,14 @@ export const ForgotPasswordText = styled.Text`
   text-align: right;
   margin: 0px 12px;
 `;
+export const RegisterText = styled.Text`
+  text-align: center;
+`
+export const RegisterLink = styled.Text`
+  color: #af65e2;
+`
+export const LetsBegin=styled.Text`
+  font-size: 24px;
+  font-weight: 400px;
+  margin: 81px 0px 24px;
+`
--- src/pages/Login/style.tsx
+++ src/pages/Login/style.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components/native";
 
 export const LoginBox = styled.View`
     display: flex;
+    justify-content: center;
     height: 100%;
 
 `
