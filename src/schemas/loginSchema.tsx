3fc85ed8ffb8b4850953d0558e1bbd8d28e42aba
Luciano <ljr.luciano.junior@outlook.com>
2024-03-08 14:56:58 -0300

    fix: Add custom regex to fix email input error
    fix: Increase password character to 8 from 6

--- src/schemas/loginSchema.tsx
+++ src/schemas/loginSchema.tsx
@@ -1,12 +1,16 @@
-import * as yup from "yup"
+import * as yup from 'yup';
+
+const isValidEmail = (email: string) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
 export const loginSchema = yup.object().shape({
   email: yup
     .string()
-    .required("O email não pode ser vazio")
-    .email("Digite um email válido"),
+    .required('O email não pode ser vazio')
+    .test('is-email', 'Digite um email válido', (value) => isValidEmail(value)),
   password: yup
     .string()
-    .required("A senha não pode ser vazia")
-    .min(6, "A senha deve conter pelo menos 6 dígitos"),
+    .required('A senha não pode ser vazia')
+    .min(8, 'A senha deve conter pelo menos 8 dígitos')
 });
