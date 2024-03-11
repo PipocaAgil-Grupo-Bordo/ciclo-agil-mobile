5e5c4473ec462155e5ce42952fafb7e4721f1de9
= <=>
2024-03-10 21:46:34 -0300

    fix: adjust input margin

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { ErrorMessage, LoginInput } from './style';
-import { Control, FieldErrors, useController } from 'react-hook-form';
-import { loginObject } from '../../types/loginType';
+import React from "react";
+import { ErrorMessage, InputContainer, LoginInput } from "./style";
+import { Control, FieldErrors, useController } from "react-hook-form";
+import { loginObject } from "../../types/loginType";
 
 interface InputProps {
-  name: 'email' | 'password';
+  name: "email" | "password";
   control: Control<loginObject>;
   errors: FieldErrors<loginObject>;
 }
 
 const Input: React.FC<InputProps> = ({ name, control, errors }) => {
-  const { field } = useController({ control, defaultValue: '', name });
+  const { field } = useController({ control, defaultValue: "", name });
 
   return (
-    <>
+    <InputContainer>
       <LoginInput
         value={field.value}
         onChangeText={field.onChange}
         errors={errors}
-        textAlign='center'
+        textAlign="center"
         name={name}
-        secureTextEntry={name === 'password' && true}
+        secureTextEntry={name === "password" && true}
       />
-      <ErrorMessage>{errors && errors[`${name}`] && errors[`${name}`]?.message}</ErrorMessage>
-    </>
+      <ErrorMessage>
+        {errors && errors[`${name}`] && errors[`${name}`]?.message}
+      </ErrorMessage>
+    </InputContainer>
   );
 };
 
