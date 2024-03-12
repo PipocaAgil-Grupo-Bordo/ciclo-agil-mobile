2bf5c580ce4462bc8db30c3814080b2c208589f0
Luciano <ljr.luciano.junior@outlook.com>
2024-03-12 18:29:08 -0300

    fix: Replace aria-hidden with the mobile attribute

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,25 +8,34 @@ import {
 import { TouchableOpacityProps } from 'react-native-gesture-handler';
 import { ReactNode } from 'react';
 
-interface ButtonProps extends ButtonStyleProps, TouchableOpacityProps{
+interface ButtonProps extends ButtonStyleProps, TouchableOpacityProps {
   children: ReactNode;
   icon?: ImageSourcePropType;
 }
 
 /**
  * Generic button to ensure style consistency across components
- * 
+ *
  * It adds the style of all current button designs that take up the whole screen's width.
- * 
+ *
  * If you need to add spacing between this button and another element, you can wrap it around with a View component and style that instead.
- * 
+ *
  * If you need to add an image, do so with the icon prop:
  * icon={require('@/assets/image.format')}
  */
 const Button: React.FC<ButtonProps> = ({ icon, state, children, ...rest }) => {
   return (
-    <StyledButton state={state} {...rest}>
-      {icon && <ButtonIcon aria-hidden={true} source={icon} />}
+    <StyledButton
+      state={state}
+      {...rest}
+    >
+      {icon && (
+        <ButtonIcon
+          importantForAccessibility='no'
+          accessibilityElementsHidden={true}
+          source={icon}
+        />
+      )}
       <ButtonText state={state}>{children}</ButtonText>
     </StyledButton>
   );
