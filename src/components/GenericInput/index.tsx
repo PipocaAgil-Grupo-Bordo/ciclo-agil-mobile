b7688a4c993216e365675e7a6340d5816a2dc700
Luciano <ljr.luciano.junior@outlook.com>
2024-03-13 20:15:30 -0300

    doc: Document the re-usable input

--- src/components/GenericInput/index.tsx
+++ src/components/GenericInput/index.tsx
@@ -4,6 +4,13 @@ import { TextInputProps } from "react-native";
 
 interface GenericInputProps extends TextInputProps {}
 
+/**
+ * Generic text input to ensure style consistency across components
+ *
+ * If you need to style it further, you can import just the styleSheet like so:
+ * import StyledInput from "./path";
+ * export const AnotherInput = styled(StyledInput)` // rest of the styles `
+ */
 const GenericInput: React.FC<GenericInputProps> = ({ ...rest }) => {
   return <StyledInput {...rest} />;
 };
