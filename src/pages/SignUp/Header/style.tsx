034d19ba6820a02480b31ce6dd4dedbbec70bde7
Luciano <ljr.luciano.junior@outlook.com>
2024-03-16 23:53:06 -0300

    feat: Add cadastrar button
    refactor: Move inputs to own file and map through them

--- src/pages/SignUp/Header/style.tsx
+++ src/pages/SignUp/Header/style.tsx
@@ -2,18 +2,15 @@ import styled from "styled-components/native";
 import { TextLora } from "../../../components/TopicText/style";
 import { StyledText } from "../../../components/TextBox/style";
 
+export const StyledContainer = styled.View`
+  align-items: center;
+`;
+
 export const StyledTopicName = styled(TextLora)`
   font-size: 24px;
-  
   margin: 32px 0px;
 `;
 
 export const StyledInstructionText = styled(StyledText)`
   font-size: 16px;
-  
-`;
-
-export const Container = styled.View`
-  
-  align-items: center;
 `;
