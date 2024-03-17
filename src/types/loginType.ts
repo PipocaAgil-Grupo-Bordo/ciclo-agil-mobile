d8b1adbc3e0328f8843e654b4a74e707d8eb47ce
Luciano <ljr.luciano.junior@outlook.com>
2024-03-16 23:29:04 -0300

    feat: Add validation register schema

--- src/types/loginType.ts
+++ src/types/loginType.ts
@@ -1,4 +1,12 @@
-export type loginObject = {
+export interface emailObject {
   email: string;
+}
+
+export interface loginObject extends emailObject {
   password: string;
-};
+}
+
+export interface registerObject extends loginObject {
+  userName: string;
+  birthDate: string;
+}
