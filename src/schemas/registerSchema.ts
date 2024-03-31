017a238f7fb1e78d1d43fff86073b3b473a257d7
Luciano <ljr.luciano.junior@outlook.com>
2024-03-31 12:59:09 -0300

    fix: Fix name not accepting white space

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 import { isValidEmail } from "./emailSchema";
 
-// Name format: Accept only letters, ignore numbers, symbols, emojis
-const nameRegex = /^[a-zA-Z]+$/;
+// Accept only letters and whitespace, ensuring there are at least two letters present
+const nameRegex = /^(?=(?:.*[a-zA-Z]){2})[a-zA-Z\s]+$/;
 
-// Date format: Accept only numbers, not letters nor symbols
+// Accept only numbers, not letters nor symbols
 const dateRegex = /^(0[1-9]|[1-2][0-9]|3[0-1])(0[1-9]|1[0-2])(19\d{2}|20(?:[0-1][0-9]|20|24))$/;
 
 // Password format (at least one uppercase letter, one lowercase, one number, and one symbol)
