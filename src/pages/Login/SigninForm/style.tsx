3d17f26bc56dc5c03d5d0fdb395e4dde2e6f9416
Luciano <ljr.luciano.junior@outlook.com>
2024-04-03 09:24:05 -0300

    chore: Remove unused previous styled components

--- src/pages/Login/SigninForm/style.tsx
+++ src/pages/Login/SigninForm/style.tsx
@@ -31,30 +31,3 @@ export const Sc = {
   `
 };
 
-export const StyledFormContainer = styled.View``;
-
-export const StyledTitle = styled(StyledText)`
-  font-size: 24px;
-  font-weight: 400;
-  margin: 81px 0px 24px;
-`;
-
-export const StyledForgottenPassword = styled(StyledText)`
-  color: #af65e2;
-  text-align: right;
-  margin: 0px 12px;
-`;
-
-export const StyledLoginWrapper = styled.View`
-  margin: 34px 0px 24px;
-`;
-
-export const StyledRegisterWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`;
-
-export const StyledRegisterLink = styled(StyledText)`
-  color: #af65e2;
-`;
