2e11381056fadd0f0fa4d759fb02c624ce6f15fd
PedroRondelli <pedrorondelli@hotmail.com>
2024-04-11 16:18:19 -0300

    install react-native-vector-icons dependency  and import icon on Policy page

--- src/pages/Policy/index.tsx
+++ src/pages/Policy/index.tsx
@@ -1,14 +1,14 @@
-import TextBox from "@components/TextBox";
 import { Sc } from "./style";
 import { Gs } from "src/styles/globalStyles";
 import Terms from "./Terms";
-
+import Entypo from "react-native-vector-icons/Entypo";
 const Policy: React.FC = () => {
   return (
     <Sc.Container nestedScrollEnabled contentContainerStyle={{ flexGrow: 1 }}>
       <Sc.Wrapper>
+        <Entypo name="chevron-left" size={20} color="#9BABB7" />
         <Gs.Title>{`Termos de Uso e \nPolítica de Privacidade`}</Gs.Title>
-        <Terms/>
+        <Terms />
       </Sc.Wrapper>
     </Sc.Container>
   );
