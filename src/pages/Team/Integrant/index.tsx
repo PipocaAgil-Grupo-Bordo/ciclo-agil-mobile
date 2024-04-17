d514fbbc168f61db6c7ea14da5b939142dee0480
PedroRondelli <pedrorondelli@hotmail.com>
2024-04-17 16:10:21 -0300

    doc: create component folders Squad, Integrant and Role

---
+++ src/pages/Team/Integrant/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Integrant: React.FC = () => {
+  return <View />;
+}
+
+export default Integrant;
---
+++ src/pages/Team/Squad/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Squad: React.FC = () => {
+  return <View />;
+}
+
+export default Squad;
