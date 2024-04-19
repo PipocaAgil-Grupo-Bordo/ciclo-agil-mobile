d014aefccdf8e1609702da52b455f7b3a2a8fb3c
PedroRondelli <pedrorondelli@hotmail.com>
2024-04-19 08:09:22 -0300

    feat: destructures and uses the Role functional component parameter

--- src/pages/Team/Role/index.tsx
+++ src/pages/Team/Role/index.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 
 import { Sc } from "./style";
+import { RolesData } from "./type";
+import Integrant from "../Integrant";
 
-const Role: React.FC = () => {
+const Role: React.FC<RolesData> = ({ title, integrants }) => {
   return (
     <Sc.Container>
       <Sc.Wrapper>
-        <Sc.RoleTitle></Sc.RoleTitle>
+        <Sc.RoleTitle>{title}</Sc.RoleTitle>
+        {integrants.map((element, i) => (
+          <Integrant
+            key={i}
+            photo={element.image}
+            name={element.name}
+            link={element.linkedIn}
+          ></Integrant>
+        ))}
       </Sc.Wrapper>
     </Sc.Container>
   );
