25cd1105a433c77c50b58abc01d1247fb31204fb
Luciano <ljr.luciano.junior@outlook.com>
2024-04-19 11:13:20 -0300

    fix: Undo header changes, as it was a test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { Sc } from "./style";
 import TopicText from "@components/TopicText";
-import Entypo from "react-native-vector-icons/Entypo";
 import { HeaderProps } from "./type";
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
@@ -10,13 +9,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <Sc.Container>
       <Sc.BackButton onPress={() => navigation.goBack()}>
-        <Entypo
-          style={{ position: "absolute", top: 0, left: -10 }}
-          name="chevron-left"
-          size={40}
-          color="#9BABB7"
-          onPress={() => navigation.goBack()}
-        />
+        <Sc.BackIcon source={require("@images/back-arrow.png")} />
       </Sc.BackButton>
 
       <Sc.Title>
