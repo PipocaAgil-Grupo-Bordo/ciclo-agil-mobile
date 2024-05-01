c993bc7fc2d9a11a2679554dd4b1d016bb3abcfb
Luciano <ljr.luciano.junior@outlook.com>
2024-05-01 17:58:09 -0300

    doc: Document what the custom hook does

--- src/hooks/useBackButtonExit.ts
+++ src/hooks/useBackButtonExit.ts
@@ -2,9 +2,14 @@ import { useCallback } from "react";
 import { useFocusEffect } from "@react-navigation/core";
 import { BackHandler } from "react-native";
 
+/**
+ * Prevent both OS and custom back buttons from going back.
+ * When called, the app will be minimized instead.
+ */
 const useBackButtonExit = () => {
   useFocusEffect(
     useCallback(() => {
+      // Handle the back button press event, exiting the app
       const onBackPress = () => {
         BackHandler.exitApp();
         return true;
@@ -12,6 +17,7 @@ const useBackButtonExit = () => {
 
       const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
 
+      // Cleanup to remove eventListener when the component unmounts
       return () => subscription.remove();
     }, [])
   );
