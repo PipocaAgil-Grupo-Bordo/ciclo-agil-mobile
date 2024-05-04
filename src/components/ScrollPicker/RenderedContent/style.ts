decf8a7410062869097108e640dc9517a8af1baf
Luciano <ljr.luciano.junior@outlook.com>
2024-05-04 19:18:14 -0300

    fix: Correct scroll getting out of sync as you scroll down

--- src/components/ScrollPicker/RenderedContent/style.ts
+++ src/components/ScrollPicker/RenderedContent/style.ts
@@ -3,7 +3,7 @@ import { isSelectedTextStyle } from "../type";
 
 export const Sc = {
   Text: styled.Text<isSelectedTextStyle>`
-    font-size: 32px;
+    font-size: 26px;
     font-family: "Montserrat";
     text-align: center;
     color: ${({ isSelected }) => (isSelected ? "#8e37c9" : "#AFB4BD")};
