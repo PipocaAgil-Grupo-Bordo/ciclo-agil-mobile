d9a39e686df99de9af775199082c3b598f8bd32a
Luciano <ljr.luciano.junior@outlook.com>
2024-05-13 08:33:47 -0300

    feat: Cycle curation screen now sends information to backend

--- src/pages/MenstrualInitialSettings/3-CycleDuration/index.tsx
+++ src/pages/MenstrualInitialSettings/3-CycleDuration/index.tsx
@@ -6,6 +6,11 @@ import Buttons from "../SharedComponents/Buttons";
 import Information from "../SharedComponents/Information";
 import DropdownMenu from "../SharedComponents/DropdownMenu";
 import ScrollableMenu from "../SharedComponents/ScrollableMenu";
+import { menstrualApi } from "@services/menstrualApi";
+import { useTokenContext } from "@context/useUserToken";
+import Modal from "@components/Modal";
+import { useNavigation } from "@react-navigation/native";
+import { NavigationType } from "@routes/type";
 
 const CycleDuration: React.FC = () => {
   // Constants
@@ -13,14 +18,21 @@ const CycleDuration: React.FC = () => {
   const IRREGULAR = 7;
 
   const cycles: CyclesType[] = ["Regular", "Irregular"];
-  const regularCycle = Array.from({ length: 35 - REGULAR + 1 }, (_, index) => index + 21);
-  const irregularCycle = Array.from({ length: 120 - IRREGULAR + 1 }, (_, index) => index + 7);
+  const regularCycle = Array.from({ length: 35 - REGULAR + 1 }, (_, index) => index + REGULAR);
+  const irregularCycle = Array.from(
+    { length: 120 - IRREGULAR + 1 },
+    (_, index) => index + IRREGULAR
+  );
 
   const [isLoading, setIsLoading] = useState(false);
   const [cycleData, setCycleData] = useState<CycleDataType>({
     cycle: "Regular",
     duration: REGULAR
   });
+  const [showModal, setShowModal] = useState(false);
+
+  const { accessToken } = useTokenContext();
+  const navigation = useNavigation<NavigationType>();
 
   const handleCycleChange = (value: CyclesType) => {
     setCycleData((prevData) => ({
@@ -49,6 +61,36 @@ const CycleDuration: React.FC = () => {
     return irregularCycle;
   };
 
+  const handleNavigation = () => {
+    navigation.navigate("Home");
+
+    if (showModal) {
+      setShowModal(false);
+    }
+  };
+
+  const handleCycleSubmission = async () => {
+    try {
+      setIsLoading(true);
+
+      await menstrualApi.currentCycle(
+        {
+          isMenstrualCycleRegular: cycleData.cycle === "Regular" ? true : false,
+          menstrualCycleDuration: cycleData.duration
+        },
+        accessToken!
+      );
+
+      setIsLoading(false);
+      handleNavigation();
+    } catch (error) {
+      // Missing error for code 400 and 401 (not sure what they're for yet, need the doc updated)
+      setIsLoading(false);
+      setShowModal(true);
+      console.log(error); // #temporary for debugging
+    }
+  };
+
   return (
     <Sc.Container>
       <Sc.TopWrapper>
@@ -71,9 +113,19 @@ const CycleDuration: React.FC = () => {
 
       <Buttons
         isLoading={isLoading}
-        nextWithData={() => alert("Próxima tela com os dados selecionados.")}
-        nextWithoutData={() => alert("Próxima tela sem os dados selecionados.")}
+        nextWithData={handleCycleSubmission}
+        nextWithoutData={handleNavigation}
       />
+
+      {showModal && (
+        <Modal
+          title="Ops"
+          textContent="Algo deu errado"
+          buttonText="Avançar mesmo assim"
+          setReadyToNext={setShowModal}
+          onPress={() => navigation.navigate("Home")}
+        />
+      )}
     </Sc.Container>
   );
 };
--- src/services/menstrualApi.ts
+++ src/services/menstrualApi.ts
@@ -1,14 +1,25 @@
+import { ICurrentCycle, ILastPeriod } from "@type/menstrual";
 import api from "./api";
 
-const lastPeriod = (body: any, token: string) => {
+const lastPeriod = (body: ILastPeriod, token: string) => {
   const promise = api.post("menstrual-period", body, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
   return promise;
-}
+};
+
+const currentCycle = (body: ICurrentCycle, token: string) => {
+  const promise = api.patch("profile", body, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  return promise;
+};
 
 export const menstrualApi = {
-  lastPeriod
-}
+  lastPeriod,
+  currentCycle
+};
