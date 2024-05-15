21dc659435b63acb50d9125ad3ec311e7abee58e
PedroRondelli <pedrorondelli@hotmail.com>
2024-05-15 16:53:13 -0300

    fix: unable hardware backbutton on Team Page

--- src/hooks/useBackButtonExit.ts
+++ src/hooks/useBackButtonExit.ts
@@ -6,19 +6,31 @@ import { BackHandler } from "react-native";
  * Prevent both OS and custom back buttons from going back.
  * When called, the app will be minimized instead.
  */
-const useBackButtonExit = () => {
+
+const useBackButtonExit = (enable = true) => {
   useFocusEffect(
     useCallback(() => {
-      // Handle the back button press event, exiting the app
-      const onBackPress = () => {
-        BackHandler.exitApp();
-        return true;
-      };
+      if (!enable) {
+        const onBackPress = () => {
+          return true;
+        };
 
-      const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
+        const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
 
-      // Cleanup to remove eventListener when the component unmounts
-      return () => subscription.remove();
+        // Cleanup to remove eventListener when the component unmounts
+        return () => subscription.remove();
+      } else {
+        // Handle the back button press event, exiting the app
+        const onBackPress = () => {
+          BackHandler.exitApp();
+          return true;
+        };
+
+        const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
+
+        // Cleanup to remove eventListener when the component unmounts
+        return () => subscription.remove();
+      }
     }, [])
   );
 };
