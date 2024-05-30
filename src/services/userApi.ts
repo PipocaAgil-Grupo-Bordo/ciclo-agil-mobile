056aabc2fb238e2cbfa998387bc8560595cc944d
PedroRondelli <pedrorondelli@hotmail.com>
2024-05-30 18:08:08 -0300

    merge test

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -1,13 +1,18 @@
 import { RegisterFields, UserData } from "@type/auth";
 import api from "./api";
 import { AxiosResponse } from "axios";
-import { ICurrentCycle } from "@type/menstrual";
 
-const signUpUser = (body: Omit<RegisterFields, "confirmEmail" | "confirmPassword">) => {
+function signUpUser(body: Omit<RegisterFields, "confirmEmail" | "confirmPassword">) {
   const promise = api.post("/users", body);
   return promise;
 }
 
+function getUserProfile(token: String): Promise<AxiosResponse<UserData>> {
+  const promise = api.get("/profile", { headers: { Authorization: `Bearer ${token}` } });
+  return promise;
+}
+
 export const userApi = {
-  signUpUser
+  signUpUser,
+  getUserProfile
 };
