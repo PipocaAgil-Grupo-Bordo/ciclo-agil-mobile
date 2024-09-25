5f31dc610f8939e061d9cac9af7e22a53df370e2
Rayandson <rayandson_silva@hotmail.com>
2024-09-25 17:48:05 -0300

    cia-182: fill previous dates when gap exists

--- src/pages/Calendar/CalendarApp/index.tsx
+++ src/pages/Calendar/CalendarApp/index.tsx
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react';
-import { View, Text, Alert, Modal, Pressable } from 'react-native';
-import { Calendar, DateData, LocaleConfig } from 'react-native-calendars';
-import { Feather } from '@expo/vector-icons';
-import { styles } from './style';
-import { ColorScheme } from '@styles/globalStyles';
-import { ptBR } from '../../../utils/localeCalendarConfig';
+import { useEffect, useState } from "react";
+import { View, Text, Alert, Modal, Pressable } from "react-native";
+import { Calendar, DateData, LocaleConfig } from "react-native-calendars";
+import { Feather } from "@expo/vector-icons";
+import { styles } from "./style";
+import { ColorScheme } from "@styles/globalStyles";
+import { ptBR } from "../../../utils/localeCalendarConfig";
 import { useTokenContext } from "@context/useUserToken";
-import { menstrualApi } from '@services/menstrualApi';
-import { ICalendarDateInfo, IMenstrualPeriod } from '@type/menstrual';
+import { menstrualApi } from "@services/menstrualApi";
+import { ICalendarDateInfo, IMenstrualPeriod } from "@type/menstrual";
 
-LocaleConfig.locales['pt-br'] = ptBR;
-LocaleConfig.defaultLocale = 'pt-br';
+LocaleConfig.locales["pt-br"] = ptBR;
+LocaleConfig.defaultLocale = "pt-br";
 
 function currentCycle(cycle: string) {
-  if (cycle === 'firstDay' || cycle === 'selected') {
+  if (cycle === "firstDay" || cycle === "selected") {
     return {
       customStyles: {
         container: {
           borderWidth: 1.9,
-          borderColor: '#DCC1EE',
-          backgroundColor: '#DCC1EE',
+          borderColor: "#DCC1EE",
+          backgroundColor: "#DCC1EE"
         },
         text: {
-          color: '#000', // Text color on the selected days
-        },
-      },
+          color: "#000" // Text color on the selected days
+        }
+      }
     };
   }
   return {};
@@ -32,7 +32,7 @@ function currentCycle(cycle: string) {
 
 function CalendarApp() {
   const [selectedDates, setSelectedDates] = useState<string[]>([]);
-  const [selectedDatesInfo, setSelectedDatesInfo] = useState<{ id: number, date: string }[]>([]);
+  const [selectedDatesInfo, setSelectedDatesInfo] = useState<{ id: number; date: string }[]>([]);
   const { accessToken } = useTokenContext();
   const [modalVisible, setModalVisible] = useState(false);
   const [pendingDate, setPendingDate] = useState<string | null>(null); // Armazena a data para decidir se deve ser adicionada ou não.
@@ -65,22 +65,23 @@ function CalendarApp() {
         setModalVisible(true); // Mostra o modal
       } else {
         addMenstrualPeriodDate(date);
+        setSelectedDates([...selectedDates, date]);
       }
     } else {
       deleteMenstrualPeriodDate(date);
     }
   };
-  
+
   const addMenstrualPeriodDate = async (date: string) => {
-    console.log(date,selectedDates)
-    setSelectedDates([...selectedDates, date]);
     try {
       if (accessToken) {
         const response = await menstrualApi.createPeriodDate({ date }, accessToken);
-        setSelectedDatesInfo([...selectedDatesInfo, { id: response.data.id, date: response.data.date }]);
+        const data = response.data;
+        setSelectedDatesInfo([...selectedDatesInfo, { id: data.id, date: data.date }]);
+        return { id: data.id, date: data.date };
       }
     } catch (error) {
-      Alert.alert('Erro ao adicionar data, tente novamente!');
+      Alert.alert("Erro ao adicionar data, tente novamente!");
       const currentSelectedDates = [...selectedDates];
       const currentSelectedDatesInfo = [...selectedDatesInfo];
       currentSelectedDatesInfo.pop();
@@ -102,7 +103,7 @@ function CalendarApp() {
         await menstrualApi.deletePeriodDate(dateInfo.id, accessToken);
       }
     } catch (error) {
-      Alert.alert('Erro ao deletar data, tente novamente!');
+      Alert.alert("Erro ao deletar data, tente novamente!");
       setSelectedDates([...selectedDates, date]);
       setSelectedDatesInfo([...selectedDatesInfo, dateInfo]);
     }
@@ -111,52 +112,63 @@ function CalendarApp() {
   const calculateDateGap = (newDate: string) => {
     if (selectedDates.length === 0) return 0;
     const lastSelectedDate = selectedDates[selectedDates.length - 1];
-    const differenceInDays = Math.abs(
-      new Date(newDate).getTime() - new Date(lastSelectedDate).getTime()
-    ) / (1000 * 3600 * 24);
+    const differenceInDays =
+      Math.abs(new Date(newDate).getTime() - new Date(lastSelectedDate).getTime()) /
+      (1000 * 3600 * 24);
     return differenceInDays;
   };
 
-  const handleModalResponse = (response: 'yes' | 'no') => {
+  const handleModalResponse = (response: "yes" | "no") => {
     setModalVisible(false);
 
-    if (response === 'yes' && pendingDate) {
+    if (response === "yes" && pendingDate) {
       // Preencher os dias anteriores se o usuário escolher 'sim'
       fillPreviousDates(pendingDate);
-    }
-    else if (response === 'no' && pendingDate) {
+    } else if (response === "no" && pendingDate) {
       const gap = calculateDateGap(pendingDate);
 
       // Somente adicionar a data se o gap for >= 3 (novo ciclo)
       if (gap >= 3) {
         addMenstrualPeriodDate(pendingDate);
+        setSelectedDates([...selectedDates, pendingDate]);
       }
     }
 
     setPendingDate(null);
   };
 
-  const fillPreviousDates = (date: string) => {
+  const fillPreviousDates = async (date: string) => {
     const lastSelectedDate = selectedDates[selectedDates.length - 1];
     const start = new Date(lastSelectedDate);
     const end = new Date(date);
     const datesToFill: string[] = [];
+    const datesToFillInfo: { id: number; date: string }[] = [];
 
     // Incrementa a data de início para não incluir o 'start' no intervalo
     start.setDate(start.getDate() + 1);
 
     for (let d = new Date(start); d < end; d.setDate(d.getDate() + 1)) {
-      datesToFill.push(d.toISOString().split('T')[0]);
+      datesToFill.push(d.toISOString().split("T")[0]);
     }
 
-
     // Preenche as datas anteriores
-    datesToFill.forEach((dateToAdd) => {
-      addMenstrualPeriodDate(dateToAdd);
+    datesToFill.forEach(async (dateToAdd) => {
+      const response = await addMenstrualPeriodDate(dateToAdd);
+
+      if (response) {
+        datesToFillInfo.push(response);
+      }
     });
 
     // Adiciona a data final selecionada
-    addMenstrualPeriodDate(date);
+    const lastDateInfo = await addMenstrualPeriodDate(date);
+
+    if (lastDateInfo) {
+      datesToFillInfo.push(lastDateInfo);
+    }
+    
+    setSelectedDates([...selectedDates, ...datesToFill, date]);
+    setSelectedDatesInfo([...selectedDatesInfo, ...datesToFillInfo]);
   };
 
   const formatDateList = (menstrualPeriods: IMenstrualPeriod[]) => {
@@ -169,7 +181,7 @@ function CalendarApp() {
     return menstrualPeriods.flatMap((menstrualPeriod: IMenstrualPeriod) =>
       menstrualPeriod.dates.map((menstrualPeriodDate) => ({
         id: menstrualPeriodDate.id,
-        date: menstrualPeriodDate.date,
+        date: menstrualPeriodDate.date
       }))
     );
   };
@@ -177,7 +189,11 @@ function CalendarApp() {
   const handleMonthChange = async (dateInfo: ICalendarDateInfo) => {
     setSelectedDates([]);
     if (accessToken) {
-      const response = await menstrualApi.getMenstrualPeriods(dateInfo.year, accessToken, dateInfo.month);
+      const response = await menstrualApi.getMenstrualPeriods(
+        dateInfo.year,
+        accessToken,
+        dateInfo.month
+      );
       const dates = formatDateList(response.data);
       setSelectedDates(dates);
     }
@@ -185,8 +201,8 @@ function CalendarApp() {
 
   const markedDates = selectedDates.reduce((acc, date) => {
     acc[date] = {
-      ...currentCycle('selected'),
-      customStyles: currentCycle('selected').customStyles,
+      ...currentCycle("selected"),
+      customStyles: currentCycle("selected").customStyles
     };
     return acc;
   }, {} as Record<string, any>);
@@ -196,26 +212,26 @@ function CalendarApp() {
       <Calendar
         style={styles.calendar}
         markingType="custom"
-        renderArrow={(direction: 'right' | 'left') => (
+        renderArrow={(direction: "right" | "left") => (
           <Feather size={24} color="#e8e8e8" name={`chevron-${direction}`} />
         )}
         headerStyle={{
           borderBottomWidth: 1,
-          borderBottomColor: '#e8e8e8',
+          borderBottomColor: "#e8e8e8",
           paddingBottom: 10,
-          marginBottom: 10,
+          marginBottom: 10
         }}
         theme={{
           textMonthFontSize: 18,
           todayTextColor: ColorScheme.circle?.primary,
           selectedDayBackgroundColor: ColorScheme.circle?.primary,
-          selectedDayTextColor: '#000',
-          arrowColor: '#e8e8e8',
-          textDayStyle: { color: '#000' },
+          selectedDayTextColor: "#000",
+          arrowColor: "#e8e8e8",
+          textDayStyle: { color: "#000" },
           arrowStyle: {
             margin: 0,
-            padding: 0,
-          },
+            padding: 0
+          }
         }}
         maxDate={new Date().toDateString()}
         hideExtraDays
@@ -224,9 +240,7 @@ function CalendarApp() {
         markedDates={markedDates}
       />
 
-      <Text style={styles.selected}>
-        Data selecionada: {selectedDates.join(', ')}
-      </Text>
+      <Text style={styles.selected}>Data selecionada: {selectedDates.join(", ")}</Text>
 
       {/* Modal para confirmar o preenchimento de dias anteriores */}
       <View style={styles.centeredView}>
@@ -243,14 +257,14 @@ function CalendarApp() {
               <View style={styles.buttonContainer}>
                 <Pressable
                   style={[styles.button, styles.buttonYes]}
-                  onPress={() => handleModalResponse('yes')}
+                  onPress={() => handleModalResponse("yes")}
                 >
                   <Text style={styles.textStyle}>Sim</Text>
                 </Pressable>
 
                 <Pressable
                   style={[styles.button, styles.buttonNo]}
-                  onPress={() => handleModalResponse('no')}
+                  onPress={() => handleModalResponse("no")}
                 >
                   <Text style={styles.textStyle}>Não</Text>
                 </Pressable>
