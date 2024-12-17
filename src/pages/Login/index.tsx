5e1ff40c0835279ad8977ad055d037d5385729bd
Rayandson <rayandson_silva@hotmail.com>
2024-12-16 22:06:31 -0300

    cia-286 (fix): change background color of public pages to keep the original color

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,7 +9,7 @@ function Login() {
   useBackButtonExit();
 
   return (
-    <PageContainer style={{ paddingLeft: 0, paddingRight: 0, paddingTop: 0 }}>
+    <PageContainer style={{ backgroundColor: '#F6FAFF', paddingLeft: 0, paddingRight: 0, paddingTop: 0 }}>
       <Sc.Wrapper>
         <Logo />
         <SigninForm />
