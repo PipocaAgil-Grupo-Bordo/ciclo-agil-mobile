1943b2162bd1fb304f6245c73ec62af96aec93d7
Rayandson <rayandson_silva@hotmail.com>
2024-12-18 12:16:23 -0300

    fix: remove duplicated imports

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { StyleProp, ViewStyle } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Container } from "./style";
-import { StatusBar } from "expo-status-bar";
 
 interface PageContainerProps {
   style?: StyleProp<ViewStyle>;
