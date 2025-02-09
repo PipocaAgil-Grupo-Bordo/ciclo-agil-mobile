4474a42d407b59c7c7f9649cc865cbb7fb5c7f0f
Karen Almeida <karenalmeida.lukiany@gmail.com>
2025-02-09 23:06:39 +0000

    install react-native-shadow-2 lib, delete style from GenericInput and sets shadow to the input from the lib

--- src/components/GenericInput/index.tsx
+++ src/components/GenericInput/index.tsx
@@ -4,6 +4,7 @@ import { useController } from "react-hook-form";
 import { Masks } from "react-native-mask-input";
 import { Sc } from "./style";
 import { ColorScheme } from "@styles/globalStyles";
+import { Shadow } from "react-native-shadow-2";
 
 /**
  * Text input with label and error message
@@ -14,45 +15,35 @@ function GenericInput({ label, control, name, errors, ...props }: GenericInputPr
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(field.value !== ""); // Mantém label acima se já tiver texto
-
-  <Sc.Label isFocused={isFocused || field.value !== ""}>{label}</Sc.Label>;
-  // <Sc.Label isFocused={isFocused || field.value !== `${label}`}>{label}</Sc.Label>;
+  const handleBlur = () => setIsFocused(false);
 
   return (
     <Sc.Container>
       {label && (
-        <Sc.Label style={{ zIndex: 999 }} isFocused={!!isFocused}>
+        <Sc.Label style={{ zIndex: 999 }} isFocused={isFocused}>
           {label}
         </Sc.Label>
       )}
-      <Sc.Input
-        // style={{ elevation: isFocused ? 4 : 0 }}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        textAlign="left"
-        value={field.value}
-        onChangeText={field.onChange}
-        placeholderTextColor={ColorScheme.text.tertiary}
-        name={name}
-        errors={errors}
-        mask={name === "birthdate" ? Masks.DATE_DDMMYYYY : undefined}
-        secureTextEntry={(name === "password" && true) || (name === "confirmPassword" && true)}
-        style={[
-          {
-            zIndex: 0,
-            borderWidth: 2,
-            borderColor: isFocused ? "#194ab4" : "#7e797e", // Azul escuro ao focar, cinza quando desfocado
-            borderRadius: 4,
-            elevation: isFocused ? 4 : 0, // Simula o "outline" no Android com sombra
-            shadowColor: "#194ab4", // Cor azulada para a sombra sem transparência
-            shadowOffset: { width: 0, height: 10 },
-            shadowOpacity: 1, // Torna a sombra totalmente opaca
-            shadowRadius: 6 // Intensidade da sombra no iOS
-          }
-        ]}
-        {...props}
-      />
+      <Shadow
+        disabled={isFocused ? false : true}
+        distance={4}
+        startColor="#b4d2f8"
+        style={{ borderRadius: 5 }}
+      >
+        <Sc.Input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          textAlign="left"
+          value={field.value}
+          onChangeText={field.onChange}
+          placeholderTextColor={ColorScheme.text.tertiary}
+          name={name}
+          errors={errors}
+          mask={name === "birthdate" ? Masks.DATE_DDMMYYYY : undefined}
+          secureTextEntry={(name === "password" && true) || (name === "confirmPassword" && true)}
+          {...props}
+        />
+      </Shadow>
       <Sc.Error>{inputErrors as string}</Sc.Error>
     </Sc.Container>
   );
--- src/components/GenericInput/style.ts
+++ src/components/GenericInput/style.ts
@@ -4,7 +4,7 @@ import MaskInput from "react-native-mask-input";
 import { ColorScheme, FontScheme } from "@styles/globalStyles";
 
 interface LabelProps {
-  isFocused?: boolean; // Add isFocused prop with optional boolean type
+  isFocused?: boolean;
 }
 
 export const Sc = {
@@ -12,34 +12,20 @@ export const Sc = {
     position: relative;
   `,
 
-  // Label: styled.Text<LabelProps>`
-  //   position: absolute;
-  //   top: 16px;
-  //   left: 16px;
-  //   font-size: ${FontScheme.size.default}px;
-  //   font-family: ${FontScheme.family.primaryMedium};
-  //   color: ${ColorScheme.text.primary};
-  //   opacity: ${(props) => (props.isFocused ? 0 : 1)};
-  //   transform: translateY(${(props) => (props.isFocused ? "-12px" : "0px")});
-  //   transition: all 0.2s ease-in-out;
-  // `,
   Label: styled.Text<LabelProps>`
     position: absolute;
-    /* top: ${({ isFocused }) => (isFocused ? "6px" : "16px")}; */
     top: -10px;
     left: 16px;
     font-size: ${FontScheme.size.default}px;
     font-family: ${FontScheme.family.primaryMedium};
     color: #49454f;
     background-color: #fafcff;
-    /* opacity: ${({ isFocused }) => (isFocused ? 1 : 0.5)}; */
-    /* transform: translateY(${({ isFocused }) => (isFocused ? "-16px" : "-35px")});
-    transition: all 0.2s ease-in-out; */
-    padding: 0 4px; /* Pequeno padding para evitar corte da label */
+    padding: 0 4px;
     z-index: 999;
   `,
 
   Input: styled(MaskInput)<StyledInputProps>`
+    width: 300px;
     position: relative;
     background-color: #fffefe;
     font-family: ${FontScheme.family.primary};
@@ -47,10 +33,11 @@ export const Sc = {
     border-radius: 5px;
     padding: 16px;
     font-size: ${FontScheme.size.default}px;
-    /* outline: ${({ isFocused }) => (isFocused ? "solid blue" : "none")}; */
     border: ${(props) =>
       props.errors && props.errors[props.name]
-        ? `0.5px solid ${ColorScheme.accent.danger}`
+        ? `1px solid ${ColorScheme.accent.danger}`
+        : props.isFocused
+        ? "#194ab4"
         : "#7e797e"};
     z-index: 0;
   `,
