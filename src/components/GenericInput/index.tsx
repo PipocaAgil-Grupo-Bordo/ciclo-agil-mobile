5705dabb2fca574c0573bd2bbce094782f962607
Karen Almeida <karenalmeida.lukiany@gmail.com>
2025-02-09 16:19:18 +0000

    fixes label to input border and changes input border style

--- src/components/GenericInput/index.tsx
+++ src/components/GenericInput/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GenericInputProps } from "./type";
 import { useController } from "react-hook-form";
 import { Masks } from "react-native-mask-input";
@@ -11,12 +11,25 @@ import { ColorScheme } from "@styles/globalStyles";
 function GenericInput({ label, control, name, errors, ...props }: GenericInputProps) {
   const { field } = useController({ control, defaultValue: "", name });
   const inputErrors = errors && errors[name] && errors[name]?.message;
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(field.value !== ""); // Mantém label acima se já tiver texto
+
+  <Sc.Label isFocused={isFocused || field.value !== ""}>{label}</Sc.Label>;
+  // <Sc.Label isFocused={isFocused || field.value !== `${label}`}>{label}</Sc.Label>;
 
   return (
     <Sc.Container>
-      {label && <Sc.Label>{label}</Sc.Label>}
-
+      {label && (
+        <Sc.Label style={{ zIndex: 999 }} isFocused={!!isFocused}>
+          {label}
+        </Sc.Label>
+      )}
       <Sc.Input
+        // style={{ elevation: isFocused ? 4 : 0 }}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         textAlign="left"
         value={field.value}
         onChangeText={field.onChange}
@@ -25,9 +38,21 @@ function GenericInput({ label, control, name, errors, ...props }: GenericInputPr
         errors={errors}
         mask={name === "birthdate" ? Masks.DATE_DDMMYYYY : undefined}
         secureTextEntry={(name === "password" && true) || (name === "confirmPassword" && true)}
+        style={[
+          {
+            zIndex: 0,
+            borderWidth: 2,
+            borderColor: isFocused ? "#194ab4" : "#7e797e", // Azul escuro ao focar, cinza quando desfocado
+            borderRadius: 4,
+            elevation: isFocused ? 4 : 0, // Simula o "outline" no Android com sombra
+            shadowColor: "#194ab4", // Cor azulada para a sombra sem transparência
+            shadowOffset: { width: 0, height: 10 },
+            shadowOpacity: 1, // Torna a sombra totalmente opaca
+            shadowRadius: 6 // Intensidade da sombra no iOS
+          }
+        ]}
         {...props}
       />
-
       <Sc.Error>{inputErrors as string}</Sc.Error>
     </Sc.Container>
   );
--- src/components/GenericInput/style.ts
+++ src/components/GenericInput/style.ts
@@ -3,25 +3,56 @@ import { StyledInputProps } from "./type";
 import MaskInput from "react-native-mask-input";
 import { ColorScheme, FontScheme } from "@styles/globalStyles";
 
-export const Sc = {
-  Container: styled.View``,
+interface LabelProps {
+  isFocused?: boolean; // Add isFocused prop with optional boolean type
+}
 
-  Label: styled.Text`
-    margin-bottom: 12px;
+export const Sc = {
+  Container: styled.View`
+    position: relative;
+  `,
+
+  // Label: styled.Text<LabelProps>`
+  //   position: absolute;
+  //   top: 16px;
+  //   left: 16px;
+  //   font-size: ${FontScheme.size.default}px;
+  //   font-family: ${FontScheme.family.primaryMedium};
+  //   color: ${ColorScheme.text.primary};
+  //   opacity: ${(props) => (props.isFocused ? 0 : 1)};
+  //   transform: translateY(${(props) => (props.isFocused ? "-12px" : "0px")});
+  //   transition: all 0.2s ease-in-out;
+  // `,
+  Label: styled.Text<LabelProps>`
+    position: absolute;
+    /* top: ${({ isFocused }) => (isFocused ? "6px" : "16px")}; */
+    top: -10px;
+    left: 16px;
     font-size: ${FontScheme.size.default}px;
     font-family: ${FontScheme.family.primaryMedium};
-    color: ${ColorScheme.text.primary};
+    color: #49454f;
+    background-color: #fafcff;
+    /* opacity: ${({ isFocused }) => (isFocused ? 1 : 0.5)}; */
+    /* transform: translateY(${({ isFocused }) => (isFocused ? "-16px" : "-35px")});
+    transition: all 0.2s ease-in-out; */
+    padding: 0 4px; /* Pequeno padding para evitar corte da label */
+    z-index: 999;
   `,
 
   Input: styled(MaskInput)<StyledInputProps>`
-    background-color: ${ColorScheme.background.secondary};
+    position: relative;
+    background-color: #fffefe;
     font-family: ${FontScheme.family.primary};
     color: ${ColorScheme.text.primary};
     border-radius: 5px;
     padding: 16px;
     font-size: ${FontScheme.size.default}px;
+    /* outline: ${({ isFocused }) => (isFocused ? "solid blue" : "none")}; */
     border: ${(props) =>
-      props.errors && props.errors[props.name] ? ColorScheme.accent.danger : "none"};
+      props.errors && props.errors[props.name]
+        ? `0.5px solid ${ColorScheme.accent.danger}`
+        : "#7e797e"};
+    z-index: 0;
   `,
 
   Error: styled.Text`
