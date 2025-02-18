b5489119728f16499ac2146e8978208b8bc9089b
Karen Almeida <karenalmeida.lukiany@gmail.com>
2025-02-18 20:32:36 +0000

    cia-299 (Feat): Fixes Eye Open incon

--- src/components/PasswordButton/index.tsx
+++ src/components/PasswordButton/index.tsx
@@ -1,7 +1,6 @@
 import EyeClosed from "@icons/EyeClosed.svg";
-// import EyeOpen from "@icons/EyeOpen.svg";
+import EyeOpen from "@icons/EyeOpen.svg";
 import { Sc } from "./style";
-import { useState } from "react";
 
 interface PasswordButtonProps {
   showPassword: boolean;
@@ -9,11 +8,7 @@ interface PasswordButtonProps {
 }
 
 function PasswordButton({ onPress, showPassword }: PasswordButtonProps) {
-  return (
-    <Sc.Button onPress={onPress}>
-      {showPassword ? <EyeClosed fill="red" /> : <EyeClosed />}
-    </Sc.Button>
-  );
+  return <Sc.Button onPress={onPress}>{showPassword ? <EyeOpen /> : <EyeClosed />}</Sc.Button>;
 }
 
 export default PasswordButton;
